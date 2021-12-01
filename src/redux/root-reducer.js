9d0436b83c92abc7dcb3ae2dbf33b5d0e85da179
Harold Salas S <hsalas.006@gmail.com>
2021-11-30 22:38:52 -0600

    store info in local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,18 @@
 import userReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer.js";
-
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whilelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: CartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer);
